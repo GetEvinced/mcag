eae7557e10c1b6e6d96fe3abd78f565221c7c1fe
illai <illai.zeevi@evinced.com>
2024-03-13 20:44:12 +0200

    nojekyll2

---
+++ out/_next/static/chunks/app/layout-4b1debee24fde4f5.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{4853:function(e,n,t){Promise.resolve().then(t.t.bind(t,5250,23)),Promise.resolve().then(t.t.bind(t,9285,23)),Promise.resolve().then(t.t.bind(t,8822,23))},9285:function(){}},function(e){e.O(0,[845,971,69,744],function(){return e(e.s=4853)}),_N_E=e.O()}]);
