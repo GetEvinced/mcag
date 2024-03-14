4b595e15c6082f3c05e0fb48bf75f07ecc6f69b3
illai <illai.zeevi@evinced.com>
2024-03-14 21:33:53 +0200

    Reverting config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // basePath: "/mcag",
-    // output: "export",
-    // reactStrictMode: true,
-    // images: {
-    //   loader: 'akamai',
-    //   path: '',
-    // }
+    basePath: "/mcag",
+    output: "export",
+    reactStrictMode: true,
+    images: {
+      loader: 'akamai',
+      path: '',
+    }
 };
 
 export default nextConfig;
